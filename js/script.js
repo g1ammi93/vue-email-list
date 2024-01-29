63ba96d3d76bad0a0f5da7ad769c5a5ce66f45b8
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-29 19:49:16 +0000

    Added axios function

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue;
+
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+const app = createApp({
+    data: () => ({
+        emails: []
+    }),
+    created() {
+        for (let i = 0; i <= 10; i++)
+            axios.get(endpoint).then(res => {
+                this.emails.push(res.data.response)
+            })
+    }
+});
+
+app.mount('#root')
+
